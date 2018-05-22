4e7b1e01fe9bd7975cfdb675b92cf780645e3bd7
Gabetiso <gabetiso@gmail.com>
2018-05-22 13:02:35 +0900

    ing

--- ayaya_edge.m
+++ ayaya_edge.m
@@ -1,10 +1,19 @@
 clear;
 close all;
 
-im = imread('ayaya.jpg');
-im_g = rgb2gray(im);
-BW1 = edge(im_g,'sobel');
-BW2 = edge(im_g,'canny');
+im1 = imread('matigai.png');
+im_g1 = rgb2gray(im1);
+
+im2 = imread('matigai1.png');
+im_g2 = rgb2gray(im2);
+
+BW1 = edge(im_g1,'canny');
+BW2 = edge(im_g2,'canny');
+
+BW3 = BW1 - BW2;
+%BW3 = BW1 .* (1 - BW2);
+
 figure;
-subplot(1,2,1),imshow(BW1);
-subplot(1,2,2),imshow(BW2);
+subplot(1,3,1),imshow(BW1);
+subplot(1,3,2),imshow(BW2);
+subplot(1,3,3),imshow(BW3);
